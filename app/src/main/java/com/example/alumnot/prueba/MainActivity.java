be6254c5958e8a1fbf8e8d5701ee735be678218b
Garbanzo <remendejo1@gmail.com>
2016-02-01 20:25:50 +0100

    Initial commit

---
+++ app/src/main/java/com/example/alumnot/prueba/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.alumnot.prueba;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+    public void cambiar(View v){
+        EditText nombre = (EditText) findViewById(R.id.editText);
+        EditText passw = (EditText) findViewById(R.id.editText2);
+
+
+
+        TextView nombre1 = (TextView) findViewById(R.id.textView);
+        TextView nombre2 = (TextView) findViewById(R.id.textView2);
+
+
+        nombre1.setText(nombre.getText());
+        nombre2.setText(passw.getText());
+
+
+
+
+    }
+
+
+}
